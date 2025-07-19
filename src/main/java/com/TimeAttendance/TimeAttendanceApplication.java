dcce21fbd526b3fbb5c777bb75e09a283dc64939
LamVanChuong04 <lamvanchuong04@gmail.com>
2025-07-19 10:11:39 +0700

    Initial commit

---
+++ src/main/java/com/TimeAttendance/TimeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.TimeAttendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TimeAttendance/TimeAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeAttendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
